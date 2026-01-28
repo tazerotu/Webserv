d7d23c071f25e47f0745a48bc6b6605b7a0dad3f
Timothy Tas <ttas@c2r9p1.42nice.fr>
2026-01-28 13:46:21 +0100

    update

--- pages/Alps/upload/upload.php
+++ pages/Alps/upload/upload.php
@@ -4,8 +4,8 @@
     <title>Upload File</title>
 </head>
 <body>
-    <h2>Upload a File</h2><br>
-	<h4>.jpg, .jpeg, .png, .gif, .pdf only<h4>
+    <h2>Upload a File</h2>
+	.jpg, .jpeg, .png, .gif, .pdf only <br><br>
     <form action="upload.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="uploadedFile" required>
         <br><br>
